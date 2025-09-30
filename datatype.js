b1c4d297b5e1c6fc0daa9e24026d15d0553592d8
Rohiitian-0111 <rohitchaudhari0130@gmail.com>
2025-09-30 16:37:49 +0000

    datatype

---
+++ datatype.js
@@ -0,0 +1,14 @@
+"use strict"; // treat all js code as new version
+
+  ///  alert(3+3)  // we are using nodejs not the browser
+console.log(3+3)
+let name="rohit"  
+let age= 20
+let isloggedin= true
+
+
+/// datatypes = number  , string , bool ,null, undefined, symbol
+console.log(typeof null) ///object
+console.log(typeof undefined) ///undefined
+
+
