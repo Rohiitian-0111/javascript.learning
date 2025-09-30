8d3639c700c846e795d844f02567205518f5e211
Rohiitian-0111 <rohitchaudhari0130@gmail.com>
2025-09-30 17:01:43 +0000

    conversion of dattatypes

---
+++ conversionops.js
@@ -0,0 +1,24 @@
+let score ="33abc";
+
+console.log(typeof(score));
+
+let valueInNumber= Number(score);
+console.log(typeof(valueInNumber));
+console.log(valueInNumber);
+
+
+/// "33" => 33
+//"33qab" => NaN
+//true => 1; false=.0;
+
+let isloggedIn= 1;
+let booleanIsLoggedIn= Boolean(isloggedIn);
+console.log(booleanIsLoggedIn); 
+
+//1 =>true ; 0=> false;
+/// "" => false;
+/// "rohit"=> true;
+let somenumber =45;
+let stringnumber =String(somenumber)
+console.log(stringnumber);
+console.log(typeof(stringnumber));
