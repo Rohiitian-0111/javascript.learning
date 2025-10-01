7335cc1432640b5dd37b37efe0dba0c9cc5802a5
Rohiitian-0111 <rohitchaudhari0130@gmail.com>
2025-10-01 13:46:17 +0000

    compa

---
+++ comparison.js
@@ -0,0 +1,13 @@
+console.log(null>0);  // false
+console.log(null==0);// false 
+console.log(null>=0);/// true 
+
+/// equality check and comparision act differentally 
+// comparison operators changes null to 0 value;
+
+console.log(undefined==0); //false
+console.log(undefined>=0);//false 
+console.log(undefined<0);//false
+
+/// strict check === 
+console.log("2"===2);
