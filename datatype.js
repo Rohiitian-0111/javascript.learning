7b9040e93e0da98e90ef88e92f31d873408b005c
Rohiitian-0111 <rohitchaudhari0130@gmail.com>
2025-10-01 14:08:51 +0000

    datatypesummary

--- datatype.js
+++ datatype.js
@@ -1,14 +1,53 @@
 "use strict"; // treat all js code as new version
 
   ///  alert(3+3)  // we are using nodejs not the browser
-console.log(3+3)
-let name="rohit"  
-let age= 20
-let isloggedin= true
+// console.log(3+3)
+// let name="rohit"  
+// let age= 20
+// let isloggedin= true
 
 
-/// datatypes = number  , string , bool ,null, undefined, symbol
-console.log(typeof null) ///object
-console.log(typeof undefined) ///undefined
+// /// datatypes = number  , string , bool ,null, undefined, symbol
+// console.log(typeof null) ///object
+// console.log(typeof undefined) ///undefined
+
+
+///data characterised in two type on the basic of how the data is store and excess 
+///1) priimitive
+//2)non primitive
+
+// primitive = string , number , boolean, null, symbol, undefined , bigint 
+
+ 
+
+///non primitive (reference)
+// arrays,  object , functions
+
+// const id =Symbol('124');
+// const anotherid=Symbol('124');
+// console.log(id);
+// console.log(anotherid);
+
+// console.log(id===anotherid);
+
+const bignumber =236548454
+8888855479n    // bigint 
+
+
+const heros= ["shaktiman", "naagraj","doga"]; /// array 
+
+let myobj={
+  name: "rohit",
+  age: 20,
+}
+
+const myFunc =function(){
+  console.log("hello world");
+}
+
+console.log(typeof myFunc);
+
+console.log(typeof heros);
+console.log(typeof myobj);
 
 
