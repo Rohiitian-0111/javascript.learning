e2211926ff111519940a0be8856a0f02af4fd10e
Rohiitian-0111 <rohitchaudhari0130@gmail.com>
2025-10-14 04:19:41 +0000

    funcobj

--- 3rd.js/function.js
+++ 3rd.js/function.js
@@ -11,19 +11,50 @@
 //     console.log(num1+num2);
 // }
 
-function addTwonums(num1, num2){
-    let result =num1+num2;
-    return result;
-}
-const result = addTwonums(3,4); 
-console.log("Result:",result);
 
-function loginusermessage(username){
-    if(username==undefined){
-        console.log("please enter a username");
-        return ;
+// function addTwonums(num1, num2){
+//     let result =num1+num2;
+//     return result;
+// }
+// const result = addTwonums(3,4); 
+// console.log("Result:",result);
 
-    }
-    return `${username} just logged in`
+// function loginusermessage(username){
+//     if(username==undefined){
+//         console.log("please enter a username");
+//         return ;
+
+//     }
+//     return `${username} just logged in`
+// }
+// console.log(loginusermessage("Rohit"));
+
+function calculatecarPrice(...num1){ //// ... is the rest operator give the array of the number 
+    return num1
 }
-console.log(loginusermessage());
+
+
+// console.log(calculatecarPrice(200,400, 500))
+
+const user= {
+    username: "rohit",
+    age: 20
+}
+
+function handleObject(anyobject){
+    console.log(`Useranme is ${anyobject.username} and age is ${anyobject.age}`);
+}
+
+// handleObject(user)
+handleObject({
+    username: "sai",
+    age: 20,
+
+}) /// another way to call the fuction object 
+
+const myNewArray =[200,400,100,600];
+ 
+function returnSecondValue (getarray){
+   return getarray[1]; 
+}
+console.log(returnSecondValue(myNewArray));
